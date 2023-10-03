dcb448b1dfeae051bdaaefd0a152d62b7f165566
J0eCool <count.j0ecool@gmail.com>
2023-10-02 21:37:52 -0700

    send and receive data from server

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,60 @@
-import React, { FC, useState } from "react"
+import React, { FC, useEffect, useState } from "react"
 
 interface User {
     name: string,
     color: string,
 }
 
+const LoginButton = ({user}: {user: User}) => {
+    return <button onClick={async () => {
+        let resp = await fetch('data/users.json')
+        let json = await resp.text()
+        console.log('logging in as', user.name, 'returns', json)
+    }}>
+        Login
+    </button>
+}
+
 const UserProfile = ({user}: {user: User}) => {
     let style: React.CSSProperties = {
         color: user.color
     }
-    return <div>
-        <div style={style}>{user.name}</div>
-    </div>
+    return <>
+        <div style={style}>{user.name}: <LoginButton user={user} /></div>
+    </>
+}
+
+const EditUsers = () => {
+    const usersUrl = 'data/users.json'
+    const [users, setUsers] = useState<User[]>()
+    useEffect(() => {
+        console.log('fetching user data')
+        fetch(usersUrl)
+            .then((res) => res.json())
+            .then((data: User[]) => setUsers(data))
+    }, [])
+
+    if (!users) {
+        return <>loading</>
+    }
+
+    return <>
+        <h1>Edit Users</h1>
+        <button onClick={() => {
+            fetch(usersUrl, {
+                method: 'PUT',
+                body: JSON.stringify(users, undefined, 2)
+            })
+        }}>
+            Push Changes
+        </button>
+        {users.map((user, index) =>
+            <UserProfile key={index} user={user} />)}
+    </>
 }
 
 const App: FC = () => {
-    return <UserProfile user={{name: "J0eCool", color: "red"}} />
+    return <EditUsers />
 }
 
 export default App
--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express'
+import fs from 'fs'
 import path from "path"
 
 const PORT: number = 1123
@@ -6,8 +7,31 @@ const app: Application = express()
 
 app.use(express.static(path.join(__dirname, "../public")))
 
-app.use('/', (req: Request, res: Response) => {
-    res.send("index.html")
+app.use(express.text())
+app.get('/data/:filename', (req: Request, res: Response) => {
+    const { filename } = req.params
+    const pathname = path.join(__dirname, '../data', filename)
+    fs.readFile(pathname, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            res.send(`error reading file at ${filename}`)
+        } else {
+            res.send(data)
+        }
+    })
+})
+app.put('/data/:filename', (req: Request, res: Response) => {
+    const { filename } = req.params
+    const pathname = path.join(__dirname, '../data', filename)
+    console.log(`request to put ${filename} with body=`, req.body)
+    fs.writeFile(pathname, req.body, (err) => {
+        if (err) {
+            console.error(err)
+            res.send(500)
+        } else {
+            res.sendStatus(200)
+        }
+    })
 })
 
 app.listen(PORT, (): void => {
