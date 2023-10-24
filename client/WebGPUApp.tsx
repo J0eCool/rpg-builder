d2aa9c04ce00b56fe6061979a3eaba2d99b9513e
J0eCool <count.j0ecool@gmail.com>
2023-10-24 14:52:35 -0700

    load textures into webgpu

--- client/WebGPUApp.tsx
+++ client/WebGPUApp.tsx
@@ -1,6 +1,16 @@
 import React, { FC, useEffect, useState, useRef } from "react"
 import { Vec2 } from "./Vec2"
 import { GameScene, Sprite, Time } from "./GameScene"
+import { text } from "stream/consumers"
+
+async function fetchText(url: string): Promise<string> {
+  const resp = await fetch(url)
+  return await resp.text()
+}
+async function fetchBitmap(url: string): Promise<ImageBitmap> {
+  const resp = await fetch(url)
+  return await createImageBitmap(await resp.blob())
+}
 
 /**
  * WebGPU Test App
@@ -9,32 +19,22 @@ export const WebGPUApp = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   useEffect(() => {
     const canvas = canvasRef.current
-    
-    // more interesting react-canvas stuff from here:
-    // https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
-    // of note: resizing canvas events
     if (!canvas) {
       return
     }
 
     const shared = {
       animFrameId: 0,
+      canceled: false,
     }
 
     if (!navigator.gpu) throw Error('WebGPU not supported')
     navigator.gpu.requestAdapter()
     .then(async (adapter: GPUAdapter | null) => {
+      if (shared.canceled) return
       if (!adapter) throw Error('requestAdapter failed')
       const device = await adapter.requestDevice()
-
-      async function fetchText(url: string): Promise<string> {
-        const resp = await fetch(url)
-        return await resp.text()
-      }
-      const shaderText = await fetchText('data/shader.wgsl')
-      const shaderModule = device.createShaderModule({
-        code: shaderText,
-      })
+      device.label = 'origin'
 
       const ctx = canvas.getContext('webgpu')
       if (!ctx) throw Error('no WebGPU context on canvas')
@@ -45,15 +45,19 @@ export const WebGPUApp = () => {
       })
 
       const vertices = new Float32Array([
-         0.0,  0.6, 0, 1,   1, 0, 0, 1,
-        -0.7, -0.6, 0, 1,   0, 1, 0, 1,
-         0.7, -0.6, 0, 1,   0, 0, 1, 1,
+        -0.5, -0.5, 0, 1,    0, 1,    //0, 0,
+        -0.5,  0.5, 0, 1,    0, 0,    //0, 0,
+         0.5, -0.5, 0, 1,    1, 1,    //0, 0,
+
+         0.5, -0.5, 0, 1,    1, 1,    //0, 0,
+        -0.5,  0.5, 0, 1,    0, 0,    //0, 0,
+         0.5,  0.5, 0, 1,    1, 0,    //0, 0,
       ])
       const vertexBuffer = device.createBuffer({
         size: vertices.byteLength,
         usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
       })
-      device.queue.writeBuffer(vertexBuffer, 0, vertices/*, 0, vertices.length*/)
+      device.queue.writeBuffer(vertexBuffer, 0, vertices)
 
       // describes the data layout of the vertex data
       const vertexBuffers: GPUVertexBufferLayout[] = [{
@@ -62,15 +66,34 @@ export const WebGPUApp = () => {
           offset: 0,
           format: 'float32x4',
         }, {
-          shaderLocation: 1, // color
+          shaderLocation: 1, // uv
           offset: 16,
-          format: 'float32x4',
+          format: 'float32x2',
         }] as GPUVertexAttribute[], // needed to typecheck
-        arrayStride: 32,
+        arrayStride: 24,
         stepMode: 'vertex',
       }]
 
+      const bitmap = await fetchBitmap('data/SHAPE_2096.png')
+      const texture = device.createTexture({
+        size: [bitmap.width, bitmap.height, 1],
+        format: 'rgba8unorm',
+        usage:
+          GPUTextureUsage.TEXTURE_BINDING |
+          GPUTextureUsage.COPY_DST |
+          GPUTextureUsage.RENDER_ATTACHMENT,
+      })
+      device.queue.copyExternalImageToTexture(
+        { source: bitmap },
+        { texture },
+        [bitmap.width, bitmap.height]
+      )
+
       // set up the GPU Pipeline
+      const shaderText = await fetchText('data/shader.wgsl')
+      const shaderModule = device.createShaderModule({
+        code: shaderText,
+      })
       const renderPipeline = device.createRenderPipeline({
         vertex: {
           module: shaderModule,
@@ -90,23 +113,47 @@ export const WebGPUApp = () => {
         layout: 'auto'
       })
 
-      const commandEncoder = device.createCommandEncoder()
-      const pass = commandEncoder.beginRenderPass({
-        colorAttachments: [{
-          clearValue: { r: 0.0, g: 0.2, b: 0.3, a: 1.0 },
-          loadOp: 'clear' as GPULoadOp,
-          storeOp: 'store' as GPUStoreOp,
-          view: ctx.getCurrentTexture().createView(),
-        }],
+      const sampler = device.createSampler({
+        minFilter: 'nearest',
+        magFilter: 'nearest',
       })
-      pass.setPipeline(renderPipeline)
-      pass.setVertexBuffer(0, vertexBuffer)
-      pass.draw(3)
-      pass.end()
 
-      device.queue.submit([commandEncoder.finish()])
+      const texView = texture.createView()
+      texView.label = 'image'
+      const uniformBindGroup = device.createBindGroup({
+        layout: renderPipeline.getBindGroupLayout(0),
+        entries: [{
+          binding: 0,
+          resource: sampler,
+        }, {
+          binding: 1,
+          resource: texture.createView(),
+        }]
+      })
 
+      // -----------------------------------------------------------
 
+      const draw = () => {
+        // device.label = `draw-${shared.animFrameId}`
+
+        const commandEncoder = device.createCommandEncoder()
+
+        const pass = commandEncoder.beginRenderPass({
+          colorAttachments: [{
+            clearValue: { r: 0.0, g: 0.2, b: 0.3, a: 1.0 },
+            loadOp: 'clear' as GPULoadOp,
+            storeOp: 'store' as GPUStoreOp,
+            view: ctx.getCurrentTexture().createView(),
+          }],
+        })
+        pass.setPipeline(renderPipeline)
+        pass.setBindGroup(0, uniformBindGroup)
+        pass.setVertexBuffer(0, vertexBuffer)
+        pass.draw(6)
+        pass.end()
+        
+        device.queue.submit([commandEncoder.finish()])
+      }
 
       let lastTick = performance.now()
       const frame = () => {
@@ -114,6 +161,8 @@ export const WebGPUApp = () => {
         const dT = (now - lastTick) / 1000
         lastTick = now
 
+        draw()
+        
         shared.animFrameId = requestAnimationFrame(frame)
       }
       frame()
@@ -122,11 +171,11 @@ export const WebGPUApp = () => {
     // cancel animation request when we unmount this app
     return () => {
       cancelAnimationFrame(shared.animFrameId)
+      shared.canceled = true
     }
   }, [])
 
   return <>
     <canvas ref={canvasRef} width="900px" height="900px" />
-
   </>
 }
--- data/shader.wgsl
+++ data/shader.wgsl
@@ -1,20 +1,29 @@
+struct VertexIn {
+  @location(0) position: vec4f,
+  @location(1) uv: vec2f,
+}
+
 struct VertexOut {
   @builtin(position) position : vec4f,
-  @location(0) color : vec4f
+  @location(0) uv : vec2f,
 }
 
 @vertex
-fn vertex_main(@location(0) position: vec4f,
-               @location(1) color: vec4f) -> VertexOut
-{
+fn vertex_main(in: VertexIn) -> VertexOut {
   var output : VertexOut;
-  output.position = position;
-  output.color = color;
+  output.position = in.position;
+  output.uv = in.uv.xy;
   return output;
 }
 
+@group(0) @binding(0) var mySampler: sampler;
+@group(0) @binding(1) var myTexture: texture_2d<f32>;
+
 @fragment
-fn fragment_main(fragData: VertexOut) -> @location(0) vec4f
-{
-  return fragData.color;
+fn fragment_main(fragData: VertexOut) -> @location(0) vec4f {
+  let c = textureSample(myTexture, mySampler, fragData.uv);
+  if (c.w <= 0.01) {
+    discard;
+  }
+  return c;
 }
